78c8e715eaf24bb0929f517310725fdd68622a3d
Naif Alshehri <visionacademydeveloper@gmail.com>
2019-07-30 19:28:28 +0300

    Update RegisterController.php

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -31,39 +31,27 @@ class RegisterController extends Controller
         ]);
 
         if ($user->save()) {
-            $profile = new Profile();
-            $profile->user_id = $user->id;
-            $profile->university_id = $request->input('university_id');
 
-            if ($profile->save()) {
-                $client = new GuzzleHttp\Client;
-                $response = $client->post(url('oauth/token'), [
-                    'form_params' => [
-                        'grant_type' => env('API_GRANT_TYPE'),
-                        'client_id' => env('API_CLIENT_ID'),
-                        'client_secret' => env('API_CLIENT_SECRET'),
-                        'username' => $request->email,
-                        'password' => $request->password,
-                        'scope' => '',
-                    ],
-                ]);
+            $client = new GuzzleHttp\Client;
+            $response = $client->post(url('oauth/token'), [
+                'form_params' => [
+                    'grant_type' => env('API_GRANT_TYPE'),
+                    'client_id' => env('API_CLIENT_ID'),
+                    'client_secret' => env('API_CLIENT_SECRET'),
+                    'username' => $request->email,
+                    'password' => $request->password,
+                    'scope' => '',
+                ],
+            ]);
 
-               // event(new NewUserHasRegisteredEvent($user));
+            // event(new NewUserHasRegisteredEvent($user));
 
-                return Response::json([
-                    'success' => true,
-                    'message' => 'Your account has been created successfully!',
-                    'user' => $user,
-                    'token_data' => json_decode((string) $response->getBody(), true)
-                ], 201);
-            } else {
-                return Response::json([
-                    'success' => false,
-                    'message' => 'Whoops this is embarrassing error , try again please.',
-                    'user' => null,
-                    'token_data' => null
-                ], 500);
-            }
+            return Response::json([
+                'success' => true,
+                'message' => 'Your account has been created successfully!',
+                'user' => $user,
+                'token_data' => json_decode((string) $response->getBody(), true)
+            ], 201);
         } else {
             return Response::json([
                 'success' => false,
