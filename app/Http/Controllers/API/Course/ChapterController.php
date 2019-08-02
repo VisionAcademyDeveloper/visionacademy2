73f307ca6ccae8881ab1c804c880a4f2099efdb0
Naif Alshehri <visionacademydeveloper@gmail.com>
2019-08-02 15:08:25 +0300

    Update ChapterController.php

--- app/Http/Controllers/API/Course/ChapterController.php
+++ app/Http/Controllers/API/Course/ChapterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Chapter;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Response;
+use App\Course;
 
 class ChapterController extends Controller
 {
