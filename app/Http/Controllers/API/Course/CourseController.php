f0e1184df99e7eb5e008c7579f751dddf103f44c
Naif Alshehri <visionacademydeveloper@gmail.com>
2019-08-02 20:17:11 +0300

    Update CourseController.php

--- app/Http/Controllers/API/Course/CourseController.php
+++ app/Http/Controllers/API/Course/CourseController.php
@@ -85,7 +85,7 @@ class CourseController extends Controller
 
     public function getCoursesByLoggedTeacher()
     {
-        return  request()->user();
+        return  auth('api')->user();
 
 
         $courses = Course::where('display', 1)->where('user_id', $user->id)->get();
